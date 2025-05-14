8f4e3395054eecdfd83d1fca5b41a2a0c224e004
SysteM1922 <guicostaantunes@gmail.com>
2025-05-14 01:07:33 +0100

    new_base sync frames

--- mediapipe-pose/new_base.py
+++ mediapipe-pose/new_base.py
@@ -12,14 +12,17 @@ mp_drawing = mp.solutions.drawing_utils
 time_stamp = 0
 results = None
 
+flag = True
+
 def update_results(result, output_image=None, timestamp=None):
     print(time.perf_counter() - time_stamp)
-    global results
+    global results, flag
     results = result
+    flag = True
 
 base_options = mp.tasks.BaseOptions(
-    model_asset_path="pose_landmarker_lite.task",
-    delegate=mp.tasks.BaseOptions.Delegate.GPU,
+    model_asset_path="pose_landmarker_heavy.task", # Path to the model file
+    delegate=mp.tasks.BaseOptions.Delegate.CPU, # Use GPU if available (only on Linux)
 )
 
 options = vision.PoseLandmarkerOptions(
@@ -33,22 +36,24 @@ options = vision.PoseLandmarkerOptions(
 detector = vision.PoseLandmarker.create_from_options(options)
 
 cap = cv2.VideoCapture(0)
+image = None
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Failed to read frame from camera")
         break
-
-    image = cv2.flip(frame, 1)
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-    time_stamp = time.perf_counter()
-    detector.detect_async(mp_image, timestamp_ms=int(time.time() * 1000))
+    
+    if flag:
+        flag = False
+        image = cv2.flip(frame, 1)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+        time_stamp = time.perf_counter()
+        detector.detect_async(mp_image, timestamp_ms=int(time.time() * 1000))
 
     if results is not None and results.pose_landmarks:
         utils.new_draw_landmarks(image, results.pose_landmarks[0], mp_pose.POSE_CONNECTIONS)
-
-    cv2.imshow("frame", image)
+        cv2.imshow("frame", image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
