4017c2b71c5510dafae0436ad559f857336bb650
Gui <guicostaantunes@gmail.com>
2025-07-22 01:43:50 +0100

    gym started

--- gym/home-gym-main/start.ps1
+++ gym/home-gym-main/start.ps1
@@ -12,7 +12,14 @@ Start-Process -FilePath rasa -ArgumentList 'run', '--ssl-certificate', '..\WebAp
 
 Set-Location -Path ..\WebAppAssistantV2
 Start-Process -FilePath .\start_web_app.bat
-Set-Location -Path ..\..
+
+Set-Location -Path ..\..\serverAPP
+Start-Process -FilePath python -ArgumentList 'server.py'
+
+Set-Location -Path ..\homegym
+Start-Process -FilePath flask -ArgumentList 'run', '-h', 'localhost', '--cert', 'cert.pem', '--key', 'key.pem'
+
 Start-Sleep -Seconds 3
 
 Start-Process "msedge.exe" "https://127.0.0.1:8082/index.htm"
+Start-Process "msedge.exe" "https://localhost:5000"
