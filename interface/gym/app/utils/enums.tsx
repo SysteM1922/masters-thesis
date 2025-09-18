c0316c6469a89a12046482dce95486587cf14cd0
Gui <guicostaantunes@gmail.com>
2025-09-18 20:22:43 +0100

    forgot this one

--- interface/gym/app/utils/enums.tsx
+++ interface/gym/app/utils/enums.tsx
@@ -1,6 +1,7 @@
 // Enum for exercise types
 export enum ExerciseType {
-    ARMS = 1,
-    LEGS = 2,
-    WALK = 3,
+    ARMS = "Levantamento de braços",
+    RIGHT_LEG = "Levantamento da perna DIREITA\n(cadeira)",
+    LEFT_LEG = "Levantamento da perna ESQUERDA\n(cadeira)",
+    WALK = "Caminhar",
 }
